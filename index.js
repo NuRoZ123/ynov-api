292f6ced0b9ce5010eff2ac4e5264f06019ef940
NuRoZ123 <killiangouzien44220@gmail.com>
2022-11-08 14:40:22 +0100

    CRUD sur todos

--- index.js
+++ index.js
@@ -1,21 +1,60 @@
-import http from 'http';
+import Koa from "koa";
+import Router from "@koa/router";
+import bodyParser from "koa-bodyparser";
 
-const server = http.createServer(((req, res) => {
+const app = new Koa();
+const router = new Router();
 
-    if(req.url === "/api/products" && req.method === "GET") {
-        res.writeHead(200, {"Content-type": "application/json"});
-        res.end(JSON.stringify({
-                id: 1,
-                name: "OPPO Reno 6 5G",
-                color: "Black"
-        }));
-    } else {
-        res.end(JSON.stringify({
-                    message: "Route not found error 404."
-        }));
+let products = [
+    {
+        id: 1,
+        name: "Iphone"
+    },
+    {
+        id: 2,
+        name: "Oppo"
+    },
+    {
+        id: 3,
+        name: "Samsung"
     }
-}));
+]
 
-const PORT = process.env.PORT || 8080;
+router.get("/api/todos", (ctx, next) => {
+   ctx.body = products;
+});
 
-server.listen(PORT, () => console.log(`server run: http://localhost:${PORT}`))
+router.get("/api/todos/:id", (ctx, next) => {
+    ctx.body = products.find(p => parseInt(ctx.params.id) === p.id);
+});
+
+router.post("/api/todos/:id/:name", (ctx, next) => {
+    products.push(
+        {
+            id: parseInt(ctx.params.id),
+            name: ctx.params.name
+        });
+
+    ctx.response.status = 201;
+})
+
+router.put("/api/todos/:id/:name", (ctx, next) => {
+    let product = products.find(p => parseInt(ctx.params.id) === p.id);
+    product.name = ctx.params.name;
+
+    ctx.response.status = 200;
+});
+
+router.delete("/api/todos/:id", (ctx, next) => {
+    let productIndex = products.findIndex(p => parseInt(ctx.params.id) === p.id);
+    products.splice(productIndex, 1);
+
+    ctx.response.status = 200;
+});
+
+app
+    .use(bodyParser())
+    .use(router.routes())
+    .use(router.allowedMethods());
+
+app.listen(process.env.PORT, () => console.log(`server running on http://localhost:${process.env.PORT}`))
